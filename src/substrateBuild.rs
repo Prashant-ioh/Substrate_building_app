1b15d15a1bd465d15303fa4809063c3fdad73a05
Prashant-ioh <vikky4424@gmail.com>
2023-04-23 23:01:27 +0530

    sub code

--- src/substrateBuild.rs
+++ src/substrateBuild.rs
@@ -5,3 +5,18 @@ fn set_value(x: u32) -> DispatchResult {
     MyStorage::set(x);
     Ok(())
 }
+
+
+// single storage value
+#[pallet::storage]
+
+type SomePrivateValue<T> = StorageValue<_, u32, ValueQuery>;
+
+#[pallet::storage]
+#[pallet::getter(fn some_primitive_value)]
+pub(super) type SomePrimitiveValue<T> = StorageValue<_, u32, ValueQuery>;
+
+
+#[pallet::storage]
+pub(super) type SomeComplexValue<T: Config> = StorageValue<_, T::AccountId, ValueQuery>;
+
