f0642e99430d77d9b533576c4788c4cfe7179ffb
Prashant-ioh <vikky4424@gmail.com>
2023-04-26 23:07:27 +0530

    sub code

--- src/EvenAndError.rs
+++ src/EvenAndError.rs
@@ -8,3 +8,36 @@ pub enum Event<T: Config> {
 	/// Set a value.
 	ValueSet { value: u32, who: T::AccountId },
 }
+
+
+//RuntimeEvent type is needed to aggregate them for the runtime
+#[pallet::config]
+	pub trait Config: frame_system::Config {
+		/// The overarching event type.
+		type RuntimeEvent: From<Event<Self>> + IsType<<Self as frame_system::Config>::RuntimeEvent>;
+	}
+
+
+
+    //Exposing events to your runtime
+
+    //To expose events to the runtime:
+
+    //1. Open the /runtime/src/lib.rsfile in a text editor
+   //2. Implement the RuntimeEvent type in the configuration trait for your pallet:
+    impl template::Config for Runtime {
+        type RuntimeEvent = RuntimeEvent;
+   }
+
+   //3. Add the RuntimeEvent type to the construct_runtime! macro:
+   construct_runtime!(
+    pub enum Runtime where
+     Block = Block,
+      NodeBlock = opaque::Block,
+      UncheckedExtrinsic = UncheckedExtrinsic
+    {
+   // --snip--
+      TemplateModule: template::{Pallet, Call, Storage, Event<T>},
+      //--add-this------------------------------------->
+        }
+);
