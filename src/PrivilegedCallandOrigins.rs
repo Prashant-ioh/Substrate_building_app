df44f4a4223c5c0432a5a30fee7d0836b2374522
Prashant-ioh <vikky4424@gmail.com>
2023-04-29 23:03:47 +0530

    sub code

---
+++ src/PrivilegedCallandOrigins.rs
@@ -0,0 +1,20 @@
+// The runtime origin is used by dispatchable functions to check where a call has come from
+
+//Raw origins
+//Substrate defines three raw origins which can be used in your runtime pallets:
+pub enum RawOrigin<AccountId> {
+	Root,
+	Signed(AccountId),
+	None,
+}
+
+
+//Origin call
+// Root
+proposal.dispatch(system::RawOrigin::Root.into())
+
+// Signed
+proposal.dispatch(system::RawOrigin::Signed(who).into())
+
+// None
+proposal.dispatch(system::RawOrigin::None.into())
