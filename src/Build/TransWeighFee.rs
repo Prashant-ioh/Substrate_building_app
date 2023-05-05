33c075ea0f45d3f74a1cf1179f0e30a10fabeced
Prashant-ioh <vikky4424@gmail.com>
2023-05-05 22:39:57 +0530

    sub code

--- src/Build/TransWeighFee.rs
+++ src/Build/TransWeighFee.rs
@@ -127,5 +127,5 @@ impl<T: Config> Pallet<T> {
 
 
 #[pallet::weight(T::DbWeight::get().read_writes(1,2)+ 10_000)]
-#[pallet::weight(T::DbWeight(10_000))]
+#[pallet::weight(T::DbWeight(100_000))]
 //
